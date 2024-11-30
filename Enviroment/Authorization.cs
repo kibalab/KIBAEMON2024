800f7d7c7f70617d3eaf357d32752662f36b03ac
KIBA <kjh030529@gmail.com>
2024-12-01 03:31:52 +0900

    [추가] Bot 인증 시스템 추가

---
+++ Enviroment/Authorization.cs
@@ -0,0 +1,11 @@
+﻿using Newtonsoft.Json;
+
+namespace KIBAEMON2024_CSharp.Enviroment;
+
+[Serializable]
+public class Authorization
+{
+    public static Authorization Empty => new();
+
+    public string Token { get; set; } = string.Empty;
+}
