c09a4108672735c4e19fef37b875b435bc5a2547
KIBA <kjh030529@gmail.com>
2024-12-01 03:32:06 +0900

    [추가] 기본 인증 로직 구현

---
+++ BotRunner.cs
@@ -0,0 +1,29 @@
+﻿using Discord;
+using Discord.WebSocket;
+using KIBAEMON2024_CSharp.Enviroment;
+
+namespace KIBAEMON2024_CSharp
+{
+    static class BotRunner
+    {
+        public static DiscordSocketClient Client { get; set; } = new();
+
+        public static async Task Main(string[] args)
+        {
+            BotEnvironment.Initialize();
+
+            Client = new DiscordSocketClient();
+            Client.Log += Log;
+            await Client.LoginAsync(TokenType.Bot, args.Any() ? BotEnvironment.Bots[args[0]].Authorization.Token : BotEnvironment.Bots.First().Value.Authorization.Token);
+            await Client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private static Task Log(LogMessage arg)
+        {
+            Console.WriteLine(arg.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
